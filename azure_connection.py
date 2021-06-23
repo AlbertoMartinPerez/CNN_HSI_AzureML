7c8295a2561126bfffcb03f96a1fb3191e5d4e86
Alberto <alberto.martinperez@protonmail.com>
2021-06-23 14:12:46 +0200

    New scripts to work with Azure Machine Learning

---
+++ azure_connection.py
@@ -0,0 +1,36 @@
+import azureml.core
+from azureml.core import Workspace, Experiment, Environment, ScriptRunConfig
+
+#*############################
+#* CHECK AZUREML-CORE VERSION
+#*
+print("Ready to use Azure ML", azureml.core.VERSION)
+
+
+#*##################################################
+#* CONNECT TO YOUR AZURE MACHINE LEARNING WORKSPACE
+#*
+# Store first the workspace connection information in a JSON.
+# This can be downloaded from the Azure portal or the workspace details
+# pane in Azure Machine Learning studio.
+ws = Workspace.from_config('config.json')
+print(ws.name, "loaded")
+
+#*#####################################
+#* PRINT AVAILABLE RESOURCES FROM YOUR 
+#* AZURE MACHINE LEARNING WORKSPACE
+#*
+print("Compute Resources:")
+for compute_name in ws.compute_targets:
+    compute = ws.compute_targets[compute_name]
+    print("\t", compute.name, ':', compute.type)
+
+#*#######################
+#* CREATE NEW EXPERIMENT
+#*
+experiment = Experiment(workspace=ws, name='exp01-Datasetmanager')
+
+run = experiment.start_logging()
+
+# Complete the run
+run.complete()
