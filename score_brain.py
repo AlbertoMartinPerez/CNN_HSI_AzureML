5e8978315cc6453630a1f438debde2361ac60838
Alberto <alberto.martinperez@protonmail.com>
2021-06-29 00:26:07 +0200

    Score script used in the AKS webservice. Reads raw images, apply the pre-processsing chain, create batches from the loaded image and convert them to PyTorch tensors. Then uses the batches to feed the loaded CNN model an predict a the classification map to be returned.

--- score_brain.py
+++ score_brain.py
@@ -1,21 +1,39 @@
 import json
+from json import JSONEncoder
+import io
+
 import joblib
 import torch
-
-import hsi_dataManager as hsi_dm    # Import 'hsi_dataManager.py' file as 'hsi_dm' to load use all desired functions
-import metrics as mts               # Import 'metrics.py' file as 'mts' to evluate metrics
+import numpy as np
+from PIL import Image
 
 from azureml.core.model import Model
+
+import hsi_dataManager as hsi_dm    # Import 'hsi_dataManager.py' file as 'hsi_dm' to load use all desired functions 
+import metrics as mts               # Import 'metrics.py' file as 'mts' to evluate metrics
+
+#*########################
+#* AZURE SERVICE ACTIONS
+#*
 
 # Called when the service is loaded
 def init():
     global model
     # Get the path to the deployed model file and load it
-    model_path = Model.get_model_path('Conv2DNet_test')
+    model_path = Model.get_model_path('Conv2DNet')
     model = joblib.load(model_path)
 
 # Called when a request is received
-def run(raw_image, white_ref, black_ref, patch_size, batch_size):
+def run(json_object):
+
+    # Deserialization
+    dictionary = json.loads(json_object)
+
+    raw_image = np.asarray(dictionary['raw_image'])
+    white_ref = np.asarray(dictionary['white_ref'])
+    black_ref = np.asarray(dictionary['black_ref'])
+    patch_size = dictionary['patch_size']
+    batch_size = dictionary['batch_size']
 
     # Create an instance of 'RawManager'
     rawManager = hsi_dm.RawManager(raw_image, white_ref, black_ref, patch_size = patch_size, batch_size = batch_size)
@@ -41,5 +59,46 @@ def run(raw_image, white_ref, black_ref, patch_size, batch_size):
     # Generate classification map from the predicted labels
     fig_predCube, _ = mts.get_classification_map(pred_labels=pred_labels, true_labels=None, coordenates=cube_coordenates, dims=dims, title="Cube classification Map", plot = False, save_plot = False, save_path = None, plot_gt = False, padding=rawManager.pad_margin)
 
-    # Return figure with the predicted classification map
-    return fig_predCube
+    # Convert a Matplotlib figure to a PIL Image, then cast to Numpy array
+    classification_map = np.array(fig2img(fig_predCube))
+
+    # Return serialized classification map PIL image to bytearray using hexadecimal encoding
+    return json.dumps({'classification_map': classification_map}, cls=NumpyArrayEncoder)
+    
+
+#*
+#* END AZURE SERVICE ACTIONS
+#*############################
+
+#*################
+#* fig2img method
+#*
+def fig2img(fig):
+    """Convert a Matplotlib figure to a PIL Image and return it"""
+    import io
+    buf = io.BytesIO()
+    fig.savefig(buf)
+    buf.seek(0)
+    img = Image.open(buf)
+    return img
+
+#*
+#* fig2img method
+#*################
+
+#*##########################
+#* NumpyArrayEncoder class
+#*
+class NumpyArrayEncoder(JSONEncoder):
+    """
+    Class to serialize Numpy arrays to JSON objects.
+    Other object instances are enconded by default.
+    """
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+#*
+#* END NumpyArrayEncoder class
+#*#############################
