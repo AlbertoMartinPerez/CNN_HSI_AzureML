aa16ca22255b08871759addadf7cb99c29d9612b
Alberto <alberto.martinperez@protonmail.com>
2021-06-29 15:05:20 +0200

    Changed function to convert pyplot figure to PIL to directly convert pyplot to numpy. It now includes the patient_id passed as argument to the title of the classification map.

--- score_brain.py
+++ score_brain.py
@@ -20,7 +20,7 @@ import metrics as mts               # Import 'metrics.py' file as 'mts' to evlua
 def init():
     global model
     # Get the path to the deployed model file and load it
-    model_path = Model.get_model_path('Conv2DNet')
+    model_path = Model.get_model_path('Conv2DNet_ID0033C02')
     model = joblib.load(model_path)
 
 # Called when a request is received
@@ -34,6 +34,7 @@ def run(json_object):
     black_ref = np.asarray(dictionary['black_ref'])
     patch_size = dictionary['patch_size']
     batch_size = dictionary['batch_size']
+    patient_id = dictionary['patient_id']
 
     # Create an instance of 'RawManager'
     rawManager = hsi_dm.RawManager(raw_image, white_ref, black_ref, patch_size = patch_size, batch_size = batch_size)
@@ -57,10 +58,11 @@ def run(json_object):
     pred_labels = model.predict(batch_x = cube_tensor_batch)
 
     # Generate classification map from the predicted labels
-    fig_predCube, _ = mts.get_classification_map(pred_labels=pred_labels, true_labels=None, coordenates=cube_coordenates, dims=dims, title="Cube classification Map", plot = False, save_plot = False, save_path = None, plot_gt = False, padding=rawManager.pad_margin)
+    title = "Patient " + patient_id + " classification Map"
+    fig_predCube, _ = mts.get_classification_map(pred_labels=pred_labels, true_labels=None, coordenates=cube_coordenates, dims=dims, title=title, plot = False, save_plot = False, save_path = None, plot_gt = False, padding=rawManager.pad_margin)
 
     # Convert a Matplotlib figure to a PIL Image, then cast to Numpy array
-    classification_map = np.array(fig2img(fig_predCube))
+    classification_map = fig2numpy(fig_predCube)
 
     # Return serialized classification map PIL image to bytearray using hexadecimal encoding
     return json.dumps({'classification_map': classification_map}, cls=NumpyArrayEncoder)
@@ -70,21 +72,23 @@ def run(json_object):
 #* END AZURE SERVICE ACTIONS
 #*############################
 
-#*################
-#* fig2img method
+#*##################
+#* fig2numpy method
 #*
-def fig2img(fig):
-    """Convert a Matplotlib figure to a PIL Image and return it"""
+def fig2numpy(fig):
+    """Convert a Matplotlib figure to a numpy array"""
     import io
     buf = io.BytesIO()
-    fig.savefig(buf)
+    fig.savefig(buf, format='raw', dpi=120)
     buf.seek(0)
-    img = Image.open(buf)
+    img = np.reshape(np.frombuffer(buf.getvalue(), dtype=np.uint8),
+                     newshape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1))
+    buf.close()
     return img
 
 #*
-#* fig2img method
-#*################
+#* fig2numpy method
+#*###################
 
 #*##########################
 #* NumpyArrayEncoder class
